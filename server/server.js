818a150e1e40325412d64176ffaea625615804c4
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-21 19:16:26 +0500

    Server Changes

--- server/server.js
+++ server/server.js
@@ -1,20 +1,49 @@
-// https://github.com/Shahbazali2001/mern-auth.git
-// https://github.com/shahbazali2001/mern-auth.git
+
 
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+connectDB();
 
 // Middlewares 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser({credentials : true}));
 
+app.get("/", (req, res) => {
+    res.send("Hello, API  is working fine");
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    
 });
