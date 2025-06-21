241147d2cd5a129794ea83db7a6bd6ae2251470d
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-21 19:57:57 +0500

    Login controller function created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,13 +34,15 @@ export const register = async (req, res) => {
     // return res.status(201).json({ message: "User registered successfully", success: true });
 
     // Token creation
-    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h"});
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h"});
     res.cookie("token", token, { 
       httpOnly: true, 
       secure: process.env.NODE_ENV === "production", 
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", 
       maxAge: 3600000 
     });
+
+    res.status(201).json({ message: "User registered successfully", success: true, token });
 
 
 
@@ -54,7 +56,31 @@ export const register = async (req, res) => {
 // Login a user
 export const login = async (req, res) => {
   try{
-    
+        const { email, password } = req.body;
+        if (!email || !password) {
+          return res.status(400).json({ message: "Email and password are required", success: false });
+        }
+
+        const user = await userModel.findOne({ email });
+        if (!User) {
+          return res.status(401).json({ message: "Invalid email", success: false });
+        }
+
+        const isMatch = await bycrypt.compare(password, user.password);
+        if (!isMatch) {
+          return res.status(401).json({ message: "Invalid password", success: false });
+        }
+
+        // Token creation
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.cookie("token", token, { 
+          httpOnly: true, 
+          secure: process.env.NODE_ENV === "production", 
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", 
+          maxAge: 3600000 
+        });
+
+        res.status(200).json({ message: "Login successful", success: true });
 
   }catch(error){
 
