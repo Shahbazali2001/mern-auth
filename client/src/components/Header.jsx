d1b546bd90a24a1b655b639d3bb06dec1ad66a30
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-02 01:46:33 +0500

    User Dyanmic Data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 function Header() {
+
+  const {userData} = useContext(AppContext);
+
   return (
     <div className='flex flex-col items-center justify-center h-screen text-gray-800'>
         <img src={assets.header_img} className='w-36 h-36 rounded-full mb-6' />
 
-        <h1 className='text-4xl inline-flex gap-5 mb-2 sm:text-3xl font-medium'>
-            Hey Developer  <img src={assets.hand_wave} className='w-8 aspect-square' /> 
+        <h1 className='text-4xl inline-flex gap-2 mb-2 sm:text-3xl font-medium'>
+            Hi<span className='font-bold text-blue-600'>{userData ? userData.name : "Developer"}!</span> <img src={assets.hand_wave} className='w-8 aspect-square' /> 
         </h1>
 
         <h2 className='text-3xl font-semibold mb-4 sm:text-5xl'>Welcome to my app</h2>
