951faf6da8ff70419426b0a01026feb7a0dd84cc
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-02 01:47:45 +0500

    Updates in Context

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -1,22 +1,40 @@
 import { useState } from "react";
-import { AppContext } from "./AppContext"
+import { AppContext } from "./AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-export const AppContextProvider = (props)=>{
+export const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState("");
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState(false);
-    const value = {
-        backendUrl,
-        isLoggedIn, setIsLoggedIn, 
-        user, setUser
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/user/get-user-data`);
+
+      if (data?.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data?.message || "Failed to fetch user data");
+      }
+    } catch (error) {
+      const errMsg =
+        error?.response?.data?.message ||
+        "Server error while fetching user data";
+      toast.error(errMsg);
     }
+  };
 
+  const value = {
+    backendUrl,
+    isLoggedIn,
+    setIsLoggedIn,
+    userData,
+    setUserData,
+    getUserData,
+  };
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-    
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
